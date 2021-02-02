9d146206d7b889a7f24c23e688d9ba303d2f6e8c
Elozino Egonmwan <26313070+Elozino-Egonmwan@users.noreply.github.com>
2021-02-02 15:59:23 -0700

    Update create_input_file.py

--- create_input_file.py
+++ create_input_file.py
@@ -5,7 +5,7 @@ lang='en'
 
 sp = spm.SentencePieceProcessor()
 sp.Load(os.environ['MODEL_DIR'] + '/spm.model')
-sents = ['best items by sales', 'what were my best items by sales', 'howmuch is paid on average per day in sales taxes']
+sents = ['show me total working capital "ducklingtime"', 'profits "ducklingtime"', 'cash over debt ratio']
 sp_sents = [' '.join(sp.EncodeAsPieces(sent)) for sent in sents]
 
 with open('test.src', 'wt') as fout:
