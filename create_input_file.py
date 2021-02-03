6b35e9b9bb651deb3d41f573e9524468cb2b8017
Elozino Egonmwan <26313070+Elozino-Egonmwan@users.noreply.github.com>
2021-02-03 09:52:40 -0700

    Add files via upload

--- create_input_file.py
+++ create_input_file.py
@@ -1,18 +1,28 @@
 import os
 import sentencepiece as spm
+import argparse
 
 lang='en'
 
 sp = spm.SentencePieceProcessor()
 sp.Load(os.environ['MODEL_DIR'] + '/spm.model')
-sents = ['show me total working capital "ducklingtime"', 'profits "ducklingtime"', 'cash over debt ratio']
-sp_sents = [' '.join(sp.EncodeAsPieces(sent)) for sent in sents]
 
-with open('test.src', 'wt') as fout:
-    for sent in sp_sents:
-        fout.write(sent + '\n')
+def getSents(sents):
+    #sents = ['Among other things, the developments in terms of turnover, employment, warehousing and prices are recorded.', ]
+    sp_sents = [' '.join(sp.EncodeAsPieces(sent)) for sent in sents]
 
-# we also need a dummy output file with the language tag
-with open('test.tgt', 'wt') as fout:
-    for sent in sp_sents:
-        fout.write(f'<{lang}> \n')
+    with open('test.src', 'wt') as fout:
+        for sent in sp_sents:
+            fout.write(sent + '\n')
+
+    # we also need a dummy output file with the language tag
+    with open('test.tgt', 'wt') as fout:
+        for sent in sp_sents:
+            fout.write(f'<{lang}> \n')
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--sents', nargs='+')
+    args = parser.parse_args()
+    getSents(args.sents)
