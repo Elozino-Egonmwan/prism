d7fdf9b5ceb3774290c5c86b95c289a7cbfd9d3b
Elozino Egonmwan <26313070+Elozino-Egonmwan@users.noreply.github.com>
2021-02-03 11:10:51 -0700

    Update generate_paraphrases.py

--- paraphrase_generation/generate_paraphrases.py
+++ paraphrase_generation/generate_paraphrases.py
@@ -461,6 +461,7 @@ def main(args):
         print('| Generate {} with beam={}: {}'.format(args.gen_subset, args.beam, scorer.result_string()))
 
     #return scorer
+    hypo_str= hypo_str.replace(" ","").replace("▁"," ")
     return hypo_str
 
 
